76f32cb8cf1a48eaa493df3423dd4ad732f4356d
Federico Bonelli <freddbomba@gmail.com>
2017-08-12 17:39:41 +0200

    some info about sketch files

---
+++ Pieces/readme.ml
@@ -0,0 +1,10 @@
+# sketch files
+Sketch files have been compiled in bohemian code Sketch
+It is proprietary software and mac only. They are temporary working files to share and create vectorial graphics
+that I use in the still small cicle of people involved in working on LGJ.
+
+It will be substitute with more, open standard file formats as soon as I find out what is appropriate and 
+has the same functionalities
+
+sketch site: https://www.sketchapp.com
+
